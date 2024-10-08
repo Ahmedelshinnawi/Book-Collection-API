80a39452c1b55f901335a1356e19476bbd8b24ba
Ahmed Elshinnawi <ahmedelshinnawi75@gmail.com>
2024-10-09 01:24:03 +0300

    Handling CORS

--- app.js
+++ app.js
@@ -10,6 +10,19 @@ app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use((req, res, next) =>{
+    res.header("Access-Control-Allow-Origin", "*"),
+    res.header("Acces-Control-Allow-Origin", 
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+
+    if(req.method === "OPTIONS"){
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+
+    }
+    next();
+});
+
 app.use('/books', bookRoutes);
 app.use('/collection', collectionRoutes);
 
