a300af284cf0db9b0f78969ff6489c5ee5a89a56
Ahmed Elshinnawi <ahmedelshinnawi75@gmail.com>
2024-10-09 07:15:49 +0300

    Adjust the schema

--- API/models/collection.js
+++ API/models/collection.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 
 const collectionSchema = mongoose.Schema({
-    collection:{type: mongoose.Schema.Types.ObjectId,ref: 'Book'},
-    quantity: {type: Number, default: 1}
+    collectionName: {type: String, required: true},
+    collection: [{type: String, required: true}]
 });
 
 
