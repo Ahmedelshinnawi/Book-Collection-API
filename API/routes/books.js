605001fa4d33657f57291a6a677315664d942d9e
Ahmed Elshinnawi <ahmedelshinnawi75@gmail.com>
2024-10-09 04:06:29 +0300

    Setting the CRUD operations on the books

--- API/routes/books.js
+++ API/routes/books.js
@@ -1,60 +1,109 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+
+const Book = require("../models/book");
+// const book = require("../models/book");
 
 router.get("/", (req, res, next) => {
-    res.status(200).json({
-        message: "Handling GET requests to /books"
+   Book.find()
+   .exec()
+   .then(docs => {
+         console.log(docs);
+         res.status(200).json(docs);
+   })
+   .catch(err => {
+    console.log(err);
+    res.status(500).json({
+        error:err
     });
+   });
 });
 
 router.post("/", (req, res, next) => {
-    const book = {
+    const book = new Book ({
         title: req.body.title,
         author: req.body.author
-    }
-
-    res.status(201).json({
-        message: "Handling POST requests to /books",
-        addedBook: book
-
     });
+    book.save()
+    .then(result => {
+        console.log(result);
+        res.status(201).json({
+            message: "Handling POST requests to /books",
+            addedBook: result
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        })
+    })
+   
 });
 
 
 router.get("/:bookId", (req, res, next) => {
     const id = req.params.bookId;
 
-    if(id  === 'special'){
-        res.status(200).json({
-            message: "You discovered the special ID",
-            id: id
+    Book.findById(id)
+    .exec()
+    .then(doc => {
+        console.log("From database", doc);
+       if(doc){
+        res.status(200).json(doc);
+       }
+       else{
+        res.status(404).json({
+            message: "No entry found with this ID"
         });
-    }
-    else{
-        res.status(200).json({
-            message: "You entered an some ID"
-        });
-    }
+       }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+          });
+    });
 });
 
 
 router.patch("/:bookId", (req, res, next) => {
-    res.status(200).json({
-        message: "Updated book!"
-    });
+   const id = req.params.bookId;
+   const updateOps = {};
+   for(const ops of req.body){
+    updateOps[ops.propName] = ops.value
+   }
+   Book.updateOne({ _id: id },{ $set: updateOps  })
+   .exec()
+   .then(result => {
+         console.log(result);
+       res.status(200).json(result);
+   })
+   .catch(err =>{
+    console.log(err);
+    res.status(500).json({
+        error: err
+      });
+   });
 });
+
+
 
 router.delete("/:bookId", (req, res, next) => {
-    res.status(200).json({
-        message: "Deleted book!"
-    });
-});
-
-router.post("/:bookId", (req, res, next) => {
-    res.status(201).json({
-        message: "Added book!"
-    });
-});
+    const id = req.params.bookId;
+    Book.deleteOne({ _id: id })
+      .exec()
+      .then(result => {
+        res.status(200).json(result);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({
+          error: err
+        });
+      });
+  });
 
 
 module.exports = router;
