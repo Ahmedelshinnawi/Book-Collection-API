789fda4f770b64741e459d5ed5ec01a61f8d39c6
Ahmed Elshinnawi <ahmedelshinnawi75@gmail.com>
2024-10-09 05:27:33 +0300

    Adding some validation with mongoose

--- API/routes/books.js
+++ API/routes/books.js
@@ -7,10 +7,26 @@ const Book = require("../models/book");
 
 router.get("/", (req, res, next) => {
    Book.find()
+   .select("title author finished _id")
    .exec()
    .then(docs => {
-         console.log(docs);
-         res.status(200).json(docs);
+         const response ={
+            count: docs.length,
+            books: docs.map(doc =>{
+                return {
+                    title: doc.title,
+                    author: doc.author,
+                    finished: doc.finished,
+                    _id: doc._id,
+                    request:{
+                        type: "GET",
+                        url: "http://localhost:3000/books/" + doc._id
+                    }
+                   
+                };
+            })
+         };
+         res.status(200).json(response);
    })
    .catch(err => {
     console.log(err);
@@ -23,14 +39,24 @@ router.get("/", (req, res, next) => {
 router.post("/", (req, res, next) => {
     const book = new Book ({
         title: req.body.title,
-        author: req.body.author
+        author: req.body.author,
+        finished: req.body.finished
     });
     book.save()
     .then(result => {
         console.log(result);
         res.status(201).json({
-            message: "Handling POST requests to /books",
-            addedBook: result
+            message: "Book added successfully",
+            addedBook: {
+                title: result.title,
+                author: result.author,
+                finished: result.finished,
+                _id: result._id,
+                request: {
+                    type: "GET",
+                    url: "http://localhost:3000/books/" + result._id
+                }
+            }
         });
     })
     .catch(err => {
@@ -47,11 +73,19 @@ router.get("/:bookId", (req, res, next) => {
     const id = req.params.bookId;
 
     Book.findById(id)
+    .select("title author finished _id")
     .exec()
     .then(doc => {
         console.log("From database", doc);
        if(doc){
-        res.status(200).json(doc);
+        res.status(200).json({
+            book: doc,
+            request: {
+                type: "GET",
+                url: "http://localhost:3000/books"
+            }
+        });
+        
        }
        else{
         res.status(404).json({
@@ -78,7 +112,13 @@ router.patch("/:bookId", (req, res, next) => {
    .exec()
    .then(result => {
          console.log(result);
-       res.status(200).json(result);
+       res.status(200).json({
+              message: "Book updated",
+              request: {
+                type: "GET",
+                url: "http://localhost:3000/books/" + id
+              }
+       });
    })
    .catch(err =>{
     console.log(err);
@@ -95,7 +135,14 @@ router.delete("/:bookId", (req, res, next) => {
     Book.deleteOne({ _id: id })
       .exec()
       .then(result => {
-        res.status(200).json(result);
+        res.status(200).json({
+            message: "Book deleted",
+            request: {
+                type: "POST",
+                url: "http://localhost:3000/books/",
+                data: { title: "String", author: "String", finished: "String" }
+              }
+        });
       })
       .catch(err => {
         console.log(err);
