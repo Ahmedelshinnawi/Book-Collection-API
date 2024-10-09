c9bd25733a7ec38073364c5e027c4761b5786ad5
Ahmed Elshinnawi <ahmedelshinnawi75@gmail.com>
2024-10-09 23:45:42 +0300

    Setting the login route, and add authentication using JWT

--- API/routes/user.js
+++ API/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
@@ -35,6 +36,48 @@ router.post('/signup', (req, res, next) => {
         };
     });
 });
+
+
+router.post('/login', (req,res,next) => {
+    User.find({email: req.body.email})
+    .exec()
+    .then(user =>{
+       if(user.length <1 ){
+           return res.status(401).json({
+               message: "Authentication failed"
+           });
+       }
+       else{
+         if(bcrypt.compareSync(req.body.password, user[0].password)){
+            const token = jwt.sign({
+                email: user[0].email,
+                userId: user[0]._id
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "1h"
+            }
+        );
+              return res.status(200).json({
+                message: "Authentication successful",
+                token: token
+              });
+         }
+            else{
+                return res.status(401).json({
+                    message: "Authentication failed"
+                });
+            };
+        };
+    
+})
+    .catch(err =>{
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+})
 
 router.delete('/:userId', (req, res, next) => {
     const id = req.params.userId;
