8fc37d855fb3ac435d7d8e60768af306994eda4f
Ahmed Elshinnawi <ahmedelshinnawi75@gmail.com>
2024-10-09 22:52:59 +0300

    add the route

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const connectDB = require("./server/config/db");
 
 const bookRoutes = require("./API/routes/books");
 const collectionRoutes = require("./API/routes/collection");
+const userRoutes = require("./API/routes/user");
 
 connectDB();
 
@@ -30,6 +31,7 @@ app.use((req, res, next) =>{
 
 app.use('/books', bookRoutes);
 app.use('/collection', collectionRoutes);
+app.use('/user', userRoutes);
 
 
 app.use((req, res, next) => {
